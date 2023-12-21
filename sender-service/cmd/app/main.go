f90a246d39c1c6ba0d49cda00fdd493a192b4dad
Enthreeka <n3ksmirnov@gmail.com>
2023-12-21 16:03:16 +0300

    feat: create listener with grpc

--- sender-service/cmd/app/main.go
+++ sender-service/cmd/app/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"github.com/Entreeka/sender/internal/app"
+	"github.com/Entreeka/sender/internal/config"
+	"github.com/Entreeka/sender/pkg/logger"
+)
 
+func main() {
+	log := logger.New()
+
+	cfg, err := config.New()
+	if err != nil {
+		log.Fatal("failed load config: %v", err)
+	}
+
+	if err := app.Run(cfg, log); err != nil {
+		log.Fatal("failed to run server: %v", err)
+	}
 }
--- sender-service/internal/app/app.go
+++ sender-service/internal/app/app.go
@@ -1,10 +1,32 @@
 package app
 
 import (
+	"fmt"
 	"github.com/Entreeka/sender/internal/config"
+	msgGrpc "github.com/Entreeka/sender/internal/controller/grpc"
 	"github.com/Entreeka/sender/pkg/logger"
+	pb "github.com/Entreeka/sender/proto/v1"
+	"google.golang.org/grpc"
+	"net"
 )
 
-func Run(cfg *config.Config, log *logger.Logger) {
+func Run(cfg *config.Config, log *logger.Logger) error {
+	s := grpc.NewServer()
+	urlGRPCServer := msgGrpc.NewMessageHandler(log)
 
+	pb.RegisterMessageServiceServer(s, urlGRPCServer)
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%s", cfg.GRPCServer.Port))
+	if err != nil {
+		log.Fatal("failed to listen: %v", err)
+		return err
+	}
+
+	log.Info("Starting gRPC listener on port :%s", cfg.GRPCServer.Port)
+	if err := s.Serve(lis); err != nil {
+		log.Fatal("failed to serve: %v", err)
+		return err
+	}
+
+	return nil
 }
